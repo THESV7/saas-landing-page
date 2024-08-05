5128a632cfbcd3818a3064c0f727e2973e29fd1c
THESV7 <vadhelsmit@gmail.com>
2024-08-05 12:04:17 +0530

    responsive

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 	return (
 		<div className="flex max-xl:flex-col-reverse max-xl:justify-center max-xl:items-center justify-between gap-40 max-xl:gap-20 w-full font-inter min-h-screen max-container my-20 max-xl:m-auto">
 			<div className="flex flex-col ">
-				<h1 className="font-bold text-7xl leading-[90px] max-xl:items-center">
+				<h1 className="font-bold text-7xl max-md:text-5xl leading-[90px] max-xl:items-center">
         We&rsquo;re here to Increase your Productivity
 				</h1>
 				<Image
@@ -15,11 +15,11 @@ const Hero = () => {
 					height={26}
 					className="pt-5"
 				/>
-				<p className="font-medium leading-7 text-lg pt-11 w-[450px] max-xl:w-auto">
+				<p className="font-medium leading-7 text-lg max-md:text-base pt-11 w-[450px] max-xl:w-auto">
 					Let&apos;s make your work more organize and easily using the Taskio
 					Dashboard with many of the latest featuresin managing work every day.
 				</p>
-				<div className="flex gap-5 font-medium text-lg pt-16">
+				<div className="flex gap-5 font-medium text-lg max-md:text-base pt-16">
 					<Button className="rounded-full py-5 px-7 bg-custom-green text-[#FFFFFF] ">
 						Try free trial
 					</Button>
@@ -35,7 +35,7 @@ const Hero = () => {
 				</div>
 			</div>
 
-      <div className="flex items-start justify-end max-xl:justify-center w-[700px]">
+      <div className="flex items-start justify-end max-xl:justify-center w-[700px] max-md:w-full">
       <Image
 					src="/assets/images/hero.png"
 					alt="vector"
